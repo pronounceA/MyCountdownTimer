789fd2400a6a3ff2dee9f9dff8652cbe20b2f7a7
pronounceA <zyank.wpo.zyank.wpo@gmail.com>
2023-06-18 16:14:33 +0900

    complete

--- app/src/main/java/com/example/mycountdowntimer/MainActivity.kt
+++ app/src/main/java/com/example/mycountdowntimer/MainActivity.kt
@@ -5,6 +5,10 @@ import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
+import android.widget.AdapterView
+import android.widget.SeekBar
+import android.widget.Spinner
 import com.example.mycountdowntimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -53,6 +57,53 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+        binding.spinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    timer.cancel()
+                    binding.playStop.setImageResource(
+                        R.drawable.baseline_play_arrow_24
+                    )
+                    val spinner = parent as? Spinner
+                    val item = spinner?.selectedItem as? String
+                    item?.let {
+                        if (it.isNotEmpty()) binding.timerText.text = it
+                        val times = it.split(":")
+                        val min = times[0].toLong()
+                        val sec = times[1].toLong()
+                        timer = MyCountDownTimer((min * 60 + sec) * 1000, 100)
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) { }
+            }
+        binding.seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    timer.cancel()
+                    binding.playStop.setImageResource(
+                        R.drawable.baseline_play_arrow_24
+                    )
+                    val min = progress / 60L
+                    val sec = progress % 60L
+                    binding.timerText.text = "%1d:%2$02d".format(min, sec)
+                    timer = MyCountDownTimer(progress * 1000L, 100)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) { }
+                override fun onStopTrackingTouch(seekBar: SeekBar?) { }
+            }
+        )
     }
 
     override fun onResume() {
